3df7a497b9d64e9f9ba6ae7757939e1c459e24cf
Iperithon <Iperithon@gmail.com>
2017-11-11 17:43:46 -0800

    Update v2
    Added postfix evaluation

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ Project to make a 'compiler' / 'interpreter'
 
 public class Main {
 	
-	public static boolean isInteger(String s) {
+	private static boolean isInteger(String s) {
 	    try { 
 	        Integer.parseInt(s); 
 	    } catch(NumberFormatException e) { 
@@ -39,11 +39,9 @@ public class Main {
 	            return 2;
 
 	        case '^':
-	            return 3;
-
-	        default:
-	            return -1;
+	            return 3;            
 	    }
+	    return -1;
 	}
 	
 	private static String convert(String x){
@@ -88,16 +86,47 @@ public class Main {
 		while (!st1.isEmpty()){	res.append(st1.pop()+ " "); }
 		return res.toString();
 	}
-	
+    
+	public static int postfixEvaluate(String exp) {
+	 	Stack<Integer> s = new Stack<Integer> ();
+		Scanner tokens = new Scanner(exp);
+		
+		while (tokens.hasNext()) {
+			if (tokens.hasNextInt()) {
+				s.push(tokens.nextInt());
+			} else {
+				int num2 = s.pop();
+				int num1 = s.pop();
+				String op = tokens.next();
+				
+				if (op.equals("+")) {
+					s.push(num1 + num2);
+				} else if (op.equals("-")) {
+					s.push(num1 - num2);
+				} else if (op.equals("*")) {
+					s.push(num1 * num2);
+				} else {
+					s.push(num1 / num2);
+				}
+				
+			//  "+", "-", "*", "/"
+			}
+		}
+		return s.pop();
+    }
+
 	public static void main (String[] args){
-		//Scanner sc = new Scanner(System.in);
+		int test = 0;
+		String equation = "3 - 1 * ( 6 / 2 ) + 10 / ( 1 - 4 + 5 )"; //-289.2
 		
-		//String eq;  //in take equation
-		// char[] infix, postfix; //infix array and postfix array
-		
-		//System.out.print("Please enter an Infix Equation: ");
-		//eq = sc.nextLine();
-		System.out.println(convert("a + b * ( c ^ d - e ) ^ ( f + g * h ) - i"));
+		equation = convert(equation);
+		System.out.println(equation);
+		test = postfixEvaluate(equation);
+		System.out.println("Evaluate postfix: " + test); //-310
+		test = 3 - 1 * ( 6 / 2 ) + 10 / ( 1 - 4 + 5 );
+		System.out.println("Evaluate infix: " + test); //-310
+		//test = 5 2 4 5 * + 1 / -;
+		//System.out.println(test);
 		
 	}	
 }
